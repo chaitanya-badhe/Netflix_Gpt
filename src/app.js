705d3e5558765fd0411276a8470d35a2ef41228a
chaitanya-badhe <chaitanyabadhe1411@gmail.com>
2026-06-27 12:30:02 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-6xl font-bold text-blue-500">
+        Tailwind Working 🚀
+      </h1>
+    </div>
+  );
+}
